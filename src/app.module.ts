470f282307639027d4c365d03868cffe2bad4c1a
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-09-08 11:47:49 +0200

    installtion + config ConfigModule + TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath: [`.env`] }) ,
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [],
+    synchronize: false,
+    logging: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
