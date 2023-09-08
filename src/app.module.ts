f00b56c994abd52498618e7e9c9e9f5358870eab
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-09-08 12:05:02 +0200

    installation + mise en place swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: [`.env`] }) ,
-  TypeOrmModule.forRoot({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: [`.env`] }) ,
+    TypeOrmModule.forRoot({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: +(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: +(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
     entities: [],
     synchronize: false,
     logging: true,
