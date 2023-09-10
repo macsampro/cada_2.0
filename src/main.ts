4a472577fc9452c50ce980321512c4577cfba356
Remy-P78 <remy80.dev@gmail.com>
2023-09-10 19:26:15 +0200

    recup ressource messages dans main

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
