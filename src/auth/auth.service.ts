9c2b0caa28b54969a1b4131972805a2a59516296
Remy-P78 <remy80.dev@gmail.com>
2023-09-15 14:21:26 +0200

    correction doublon auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,7 +9,7 @@ import * as bcrypt from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
-import { LoginDto } from './dto/login.dot';
+import { LoginDto } from './dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:4200', // Remplacez par le domaine que vous souhaitez autoriser
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // autorise requete avec token etc..
+  };
+   
+
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Amis des Animaux')
