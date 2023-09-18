f6c2b4c53aa998d7954e488eb230f7ab8ba31974
Remy-P78 <remy80.dev@gmail.com>
2023-09-18 09:35:06 +0200

    correction dot/dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { LoginDto } from './dto/login.dot';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
